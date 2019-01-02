459603dde9439c0d5779a233062d945101a0d8a3
Paris Ward <wardparis.pw@gmail.com>
2019-01-02 09:12:48 -0800

    began form building with redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+    //formProps, are form properties passed through redux-form
+    //can be destructured to ({input}), that contains necessary
+    //props for storing value. i.e. onChange, onBlur, value, etc.
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
+    }
 
-};
+    render() {
+        return(
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+            </form>
+        )
+    }
 
-export default StreamCreate;
+}
+//reduxForm accepts only one configuration object
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
