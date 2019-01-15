8bd8d0a9aac41ac55867b05d29e8a6903c2efb3e
Paris Ward <wardparis.pw@gmail.com>
2019-01-14 17:22:22 -0800

    created all action creators using restful conventions, began reducer building

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import { 
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
 
 export const signIn = (userId) => {
     return {
@@ -13,7 +21,40 @@ export const signOut = () => {
         type: SIGN_OUT
     };
 };
+
 //this post request using  restful conventions will create a stream 
 export const createStream = formValues => async dispatch => {
-    streams.post('/streams', formValues);
+    const response = await streams.post('/streams', formValues);
+    //dispatch a create stream action with the payload of the data from the response
+    dispatch({ type: CREATE_STREAM, payload: response.data });
 };
+
+//this get request using restful conventions will fetch a list of streams
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+//get request to fetch a single particular stream, not a list like above
+export const fetchStream = id => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+//put request to edit a stream, passing both id of stream to be edited and formValues
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+//delete request to delete a stream, again passing the id of the stream to be deleted
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+    //unlike the other action creators being dispatched, our payload
+    //will be the id of the stream we are deleting
+    dispatch({ type: DELETE_STREAM, payload: id });
+};
+
