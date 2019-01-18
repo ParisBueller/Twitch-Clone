825c1907db42a25a85ac58a58408c01327dfc1fe
Paris Ward <wardparis.pw@gmail.com>
2019-01-18 09:45:04 -0800

    programmatic navigation with custom browser history object

---
+++ src/history.js
@@ -0,0 +1,5 @@
+//history was installed with react-router-dom
+import createHistory from 'history/createBrowserHistory';
+//creates a router history object that 
+//we can access anywhere inside project
+export default createHistory();
