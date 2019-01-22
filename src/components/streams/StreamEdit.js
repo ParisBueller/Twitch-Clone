5a2afe353cd6bbddffca32d3b7d85e0d290ce866
Paris Ward <wardparis.pw@gmail.com>
2019-01-21 16:56:50 -0800

    edited edit-stream route path, added id to identify stream to be edited

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+const StreamEdit = props => {
+    console.log(props);
     return <div>StreamEdit</div>;
 
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    //ownProps.match.params.id contains the id of the stream to be edited
+    //identified and stored in redux store when the user clicked edit stream
+    return { stream: state.streams[ownProps.match.params.id]}
+};
+
+export default connect(mapStateToProps)(StreamEdit);
